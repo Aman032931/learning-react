d62e94975b214abfb9a83cf97fae28e211731c0b
Aman6231 <aman.grg.0329@gmail.com>
2026-08-22 11:10:09 +0545

    react-router

--- 46 react-router/src/components/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="d-flex justify-content-center spinner" >
-      <div className="spinner-border" role="status" style={{width: '5rem', height: '5rem'}}>
-        <span className="visually-hidden">Loading...</span>
-      </div>
-    </div>
-  );
-}
